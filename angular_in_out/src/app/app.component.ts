76ed506ab09e00ee5a88639d8eac6a4dcac83db8
jose-ortiz1 <joseluis_0396@hotmail.com>
2024-12-14 17:31:41 -0700

    connection data base

--- angular_in_out/src/app/app.component.ts
+++ angular_in_out/src/app/app.component.ts
@@ -2,24 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StudentsComponent } from "./students/students.component";
 import { Student } from './student';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StudentsComponent],
+  imports: [RouterOutlet, StudentsComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_in_out';
+  title:string = 'angular_in_out';
+  val_from_child: string = '';
 
   student:Student = {id:1, name: "Jose", age: 28}
 
-  valFromChild(val:String){
+  valFromChild(val:string){
     console.log(val, "Value came from child");
-
-
-
+    this.val_from_child = val;
 
   }
 
--- angular_in_out/src/app/students/students.component.ts
+++ angular_in_out/src/app/students/students.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { Student } from '../student';
 
 @Component({
@@ -14,11 +14,29 @@ export class StudentsComponent {
   @Input() student_obj_from_parent! : Student;
   @Output() event_to_send = new EventEmitter();
 
-
   addVal(val:string){
     //console.log(val);
     this.event_to_send.emit(val);
     
+
+  }
+
+  constructor(){
+    console.log("constructor is called");
+   
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit is called");
+    console.log("Before ", this.title_from_parent);
+
+    this.title_from_parent = "title is updated";
+    console.log("After ", this.title_from_parent);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges is called", changes);
+    console.log("Changes ", this.title_from_parent);
 
   }
 }
